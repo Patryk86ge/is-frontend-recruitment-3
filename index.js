176686a2d3af50836f3e354046afe3d1da72c4a2
Patryk86ge <kokot.patryk@gmail.com>
2022-02-14 16:40:24 +0100

    tasks3Add

--- index.js
+++ index.js
@@ -37,51 +37,69 @@ const data = {
   }
 }
 
-const result = {
-  'UK': [
-    {
-      firstName: 'Jan',
-      lastName: 'Kowalski',
-      company: {
-        name: 'Industry LTD',
-        address: '20 Williams Circle',
-        countryCode: 'UK'
-      }
-    }, {
-      firstName: 'Ann',
-      lastName: 'Tayor',
-      company: {
-        name: 'Ally Financial',
-        address: '9 Purple Finch Street',
-        countryCode: 'UK'
-      }
-    }
-  ],
-  'US': [
-    {
-      firstName: 'John',
-      lastName: 'Smith',
-      company: {
-        name: 'VxxonMobil',
-        address: '770 Cemetery Street',
-        countryCode: 'US'
-      }
-    }, {
-      firstName: 'Arnold',
-      lastName: 'Blake',
-      company: {
-        name: 'Washington Wizards',
-        address: '4 Old Brook Street',
-        countryCode: 'US'
-      }
-    }
-  ]
+// const result = {
+//   'UK': [
+//     {
+//       firstName: 'Jan',
+//       lastName: 'Kowalski',
+//       company: {
+//         name: 'Industry LTD',
+//         address: '20 Williams Circle',
+//         countryCode: 'UK'
+//       }
+//     }, {
+//       firstName: 'Ann',
+//       lastName: 'Tayor',
+//       company: {
+//         name: 'Ally Financial',
+//         address: '9 Purple Finch Street',
+//         countryCode: 'UK'
+//       }
+//     }
+//   ],
+//   'US': [
+//     {
+//       firstName: 'John',
+//       lastName: 'Smith',
+//       company: {
+//         name: 'VxxonMobil',
+//         address: '770 Cemetery Street',
+//         countryCode: 'US'
+//       }
+//     }, {
+//       firstName: 'Arnold',
+//       lastName: 'Blake',
+//       company: {
+//         name: 'Washington Wizards',
+//         address: '4 Old Brook Street',
+//         countryCode: 'US'
+//       }
+//     }
+//   ]
+// }
+
+let result = {};
+const addToResult = (el) =>  {
+  if (!el) {
+    console.error('Parameter el cannot be null');
+    return;
+  }
+  let countryCode = el.company.countryCode;
+
+  let resEl = result[countryCode];
+  if (!resEl) {
+    result[countryCode] = [];
+  }
+  result[countryCode].push(el);
 }
+Object.keys(data).reduce((prev, curr, idx, arr) => {
+  if (prev) {
+    addToResult(data[prev]);
+  }
+  addToResult(data[curr]);
+})
 
-let aa = Object.keys(data).reduce((result,key) => {
-  console.log(result[key]);
-
-}, {})
+console.log(result);
 
 
 
